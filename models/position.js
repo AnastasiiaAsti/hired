0dc2d4bd8b53c7340e736b0345efe6a065e8eaf6
Asti <astistateofmind@gmail.com>
2023-05-18 10:17:52 -0700

    apps position model

---
+++ models/position.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const positionSchema = new Schema(
+    {
+        position: {
+            type: String,
+            required: true,
+        },
+        positionUrl: String,
+        jobDetails: String,
+        skillsRequired: String,
+        skillsOptional: String,
+        location: String,
+        companyName: {
+            type: String,
+            required: true,
+        },
+        companyUrl: String,
+        industry: String,
+        companyDetails: String,
+        user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        },
+        website: {
+            type: String,
+            enum: ['linkedIn', 'Indeed', 'ZipRecruiter', 'Company website']
+        },
+    },
+    {
+      timestamps: true,
+    }
+  );
+  
+  module.exports = mongoose.model("Position", positionSchema);
